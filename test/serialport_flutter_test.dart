1e910662b35f865af82cefcfaf4f63880142e62f
Saeed Ghasemi <saeed.razii93@gmail.com>
2022-11-09 17:57:39 +0330

    fix test issues

--- test/serialport_flutter_test.dart
+++ test/serialport_flutter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:serialport_flutter/serialport_flutter.dart';
 import 'package:serialport_flutter/serialport_flutter_platform_interface.dart';
@@ -7,13 +9,36 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockSerialportFlutterPlatform
     with MockPlatformInterfaceMixin
     implements SerialportFlutterPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<bool?> close() => Future.value(true);
+
+  @override
+  Future<List<String>?> getAllDevices() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String>?> getAllDevicesPath() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool?> open(String filePath, int baudrate) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool?> write(Uint8List data) {
+    throw UnimplementedError();
+  }
 }
 
 void main() {
-  final SerialportFlutterPlatform initialPlatform = SerialportFlutterPlatform.instance;
+  final SerialportFlutterPlatform initialPlatform =
+      SerialportFlutterPlatform.instance;
 
   test('$MethodChannelSerialportFlutter is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelSerialportFlutter>());
@@ -21,7 +46,8 @@ void main() {
 
   test('getPlatformVersion', () async {
     SerialportFlutter serialportFlutterPlugin = SerialportFlutter();
-    MockSerialportFlutterPlatform fakePlatform = MockSerialportFlutterPlatform();
+    MockSerialportFlutterPlatform fakePlatform =
+        MockSerialportFlutterPlatform();
     SerialportFlutterPlatform.instance = fakePlatform;
 
     expect(await serialportFlutterPlugin.getPlatformVersion(), '42');
