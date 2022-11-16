a9c12a77a17e1bd853eda8000dc0bb40d3a670d2
Saeed Ghasemi <saeed.razii93@gmail.com>
2022-11-16 23:00:36 +0330

    chore: cleaning example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:bitmap/bitmap.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:serialport_flutter/serialport_flutter.dart';
-import 'package:serialport_flutter_example/HI.dart';
+import 'package:serialport_flutter_example/sds.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,34 +18,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  List _devices = ['hi'];
   final _serialportFlutterPlugin = SerialportFlutter();
+  final bmp = Bitmap.fromHeadless(500, 500, Uint8List.fromList(data1));
 
   @override
   void initState() {
+    getDevices();
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = await _serialportFlutterPlugin.getPlatformVersion() ??
-          'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  @override
+  void dispose() async {
+    await _serialportFlutterPlugin.close();
+    super.dispose();
+  }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
+  Future<void> getDevices() async {
+    List? devices;
+    try {
+      devices = (await _serialportFlutterPlugin.getAllDevicesPath());
+    } on PlatformException {
+      devices = [];
+    }
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _devices = devices!;
     });
   }
 
@@ -52,33 +52,48 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Plugin example app'),
-          ),
-          body: Center(
-            child: Text('Running on: $_platformVersion\n'),
-          ),
-          floatingActionButton: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              FloatingActionButton(
-                onPressed: () async {
-                  final hi =
-                      await _serialportFlutterPlugin.open("/dev/ttyS1", 115200);
-                  print(hi);
-                },
-                child: Text('open'),
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Scrollbar(
+                child: ListView(
+                  children: [
+                    // for (final device in _devices)
+                    //   Builder(builder: (context) {
+                    //     return Text(device);
+                    //   }),
+                    // SizedBox(
+                    //   height: 500,
+                    //   child: Image.memory(Uint8List.fromList(data1)),
+                    // ),
+                  ],
+                ),
               ),
-              FloatingActionButton(
-                onPressed: () async {
-                  final hi = await _serialportFlutterPlugin
-                      .write(Uint8List.fromList(data));
-                  print(hi);
-                },
-                child: Text('write'),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardListTile extends StatelessWidget {
+  const CardListTile({super.key, required this.name, required this.value});
+
+  final String name;
+  final String? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(value ?? 'N/A'),
+        subtitle: Text(name),
+      ),
     );
   }
 }
